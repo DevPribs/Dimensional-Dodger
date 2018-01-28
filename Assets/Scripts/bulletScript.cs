3650196907f73bc2729e4b3a4f3ef03f8d3e5911
DevPribs <purpribs@hotmail.com>
2018-01-27 19:31:17 -0500

    Added player hp, Added bullets

---
+++ Assets/Scripts/bulletScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour {
+
+    public int speed = 20;
+
+    // Use this for initialization
+    void Start () {
+        //Rigidbody2D rb2d = (Rigidbody2D) GetComponent("Rigidbody2D");
+
+        //rb2d.AddForce(transform.up * speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += (transform.up * speed * Time.deltaTime )/ 25;
+	}
+}
